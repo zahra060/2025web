406c2fa2fa8de543f8bd7e7ee5f144c8e231417e
Snow Sparkle <zahraazshiva@gmail.com>
2025-03-10 13:05:59 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -7,5 +7,25 @@
 </head>
 <body>
     <h1>DATA MAHASISWA</h1>
+    <br>
+    <table border ="1" cellspacing ="0" cellpadding ="5">
+        <thead>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>E020323080</td>
+                <td>Zahra</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323001</td>
+                <td>Shiva</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
